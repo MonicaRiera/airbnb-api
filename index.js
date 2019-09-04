0f5f10bd7cd310afc7b9515e1e62fbb1cfec94ed
Monica <monicarigi@hotmail.com>
2019-09-04 12:21:36 +0700

    080203

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 let app = express()
 
 app.get('/', (req, res) => res.send('Welcome to Airbnb API'))
+app.get('/places', (req, res) => {
+	res.send(
+		require('./getPlaces')
+	)
+})
+
 app.listen(4000, () => {
 	console.log('Ready on port 4000')
 })
