4d46e209b99095ed4e032ed5bd9936fdba9ff3c5
Monica <monicarigi@hotmail.com>
2019-09-07 18:42:10 +0700

    090902

--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,8 +1,20 @@
 const Place = require('../models/place')
 
 module.exports = (req, res) => {
+	filter = {}
+	if (req.query.max_price) {
+		filter.price = {$lte: req.query.max_price}
+	}
 
-	Place.find({price: {$lte: req.query.max_price}}).populate('type')
+	if (req.query.min_guests) {
+		filter.guests = {$gte: req.query.min_guests}
+	}
+	console.log(filter)
+	Place.find(filter).populate('type')
 	.then(data => res.send(data))
 	.catch(err => res.send(err))
 }
+
+
+// {price: {$lte: req.query.max_price},
+// guests: {$gte: req.query.min_guests}}
