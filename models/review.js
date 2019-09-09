4542aa9b91566b70efa640ff25cf4b37e5024693
Monica <monicarigi@hotmail.com>
2019-09-09 10:41:02 +0700

    091103

---
+++ models/review.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const Review = mongoose.model('review', {
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user',
+		required: [true, 'Author is required']
+	},
+	date: {
+		type: Date,
+		default: Date.now()
+	},
+	rating: {
+		type: Number,
+		default: 0
+	},
+	content: {
+		type: String,
+		required: [true, 'Content is required']
+	}
+})
+
+module.exports = Review
