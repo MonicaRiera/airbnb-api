77d94e88047eb7158cbadc7652ab8cf259bf1f91
Monica <monicarigi@hotmail.com>
2019-09-11 15:09:11 +0700

    100201

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,26 +1,13 @@
 const Place = require('../models/place')
 const Review = require('../models/review')
 
-const findReviews = (place) => {
-	return new Promise((res, rej) => {
-		Review.find({place: place._id}).lean()
-		.then(reviews => {
-			place.img = place.images[0]
-			place.reviews = reviews
-			place.rating = 0
-			if (reviews.length) {
-				let sum = 0
-				reviews.forEach(r => sum += r.rating)
-				place.rating = Math.round(sum / data.length)
-			}
-			res(place)
-		})
-	})
-}
-
 module.exports = (req, res) => {
 
-	Place.findOne({_id: req.params.id}).select('title images type bedrooms city country price').populate('type').populate('host', 'name avatar').populate('amenities').populate({
+	Place.findOne({_id: req.params.id})
+	.populate('type')
+	.populate('host', 'name avatar')
+	.populate('amenities')
+	.populate({
 		path: 'reviews',
 		populate: {
 			path: 'author',
@@ -29,8 +16,20 @@ module.exports = (req, res) => {
 	})
 	.lean()
 	.then(place => {
-		findReviews(place)
-		.then(place => res.send(place))
+		place.img = place.images[0]
+		place.rating = 0
+		Review.find({place: place._id}).populate('author')
+		.lean()
+		.then(reviews => {
+			console.log(reviews)
+			place.reviews = reviews
+			if (reviews.length) {
+				let sum = 0
+				reviews.forEach(r => sum += r.rating)
+				place.rating = Math.round(sum / reviews.length)
+			}
+			res.send(place)
+		})
 	})
 	.catch(err => res.send(err))
 }
