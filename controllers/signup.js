4f8e5adeac841ab2503398123ed5304463ffaaa9
Monica <monicarigi@hotmail.com>
2019-09-12 15:30:43 +0700

    120105

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,7 +1,14 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
 	User.create(req.body).then(data => {
-		res.send(data)
+		let object = data.toObject()
+		let token = jwt.sign(object, 'randomCharacters')
+		res.send({token: token})
 	})
 }
+
+// 120104
+// const bcrypt = require('bcrypt')
+// req.body.password = bcrypt.hashSync(req.body.password, 10)
