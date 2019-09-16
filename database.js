93d866effcba1448d1bd3067542dfd6afe8124d1
Monica <monicarigi@hotmail.com>
2019-09-16 11:36:37 +0700

    130102

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // let connection = 'mongodb+srv://${process.env.MONGO_ATLAS}@cluster0-ypjhv.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
-let connection = process.env.DB_URL
+let connection = process.env.MONGODB_URI
 
 mongoose.connect(connection, {useNewUrlParser:true}, (err) => {
 	if (err) {
